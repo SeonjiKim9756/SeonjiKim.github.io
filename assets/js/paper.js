b2d3ecff52c41d06e73ce937bbeab0e8d6b3506a
SeonjiKim9756 <mikosun97@gmail.com>
2026-07-22 16:57:42 +0900

    .

--- assets/js/paper.js
+++ assets/js/paper.js
@@ -1,5 +1,5 @@
 (() => {
-  const CACHE_BUST = "20260722f";
+  const CACHE_BUST = "20260722h";
   const owner = "Seonji Kim";
   const siteStamp = document.getElementById("site-stamp");
   const bibtexCopyButton = document.getElementById("paper-bibtex-copy");
