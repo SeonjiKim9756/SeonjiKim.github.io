2cff5efadc92c72693207604d8122a6f9dea6d1b
SeonjiKim9756 <mikosun97@gmail.com>
2026-07-23 20:32:23 +0900

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 (() => {
-  const CACHE_BUST = "20260723c";
+  const CACHE_BUST = "20260723d";
   const owner = "Seonji Kim";
   const list = document.getElementById("publication-list");
   const heroMeta = document.getElementById("hero-meta");
